7f9f001906056e198cd8d6fecadf732e11ed53a6
Dinuvi9170 <dinuvitharushika@gmail.com>
2025-12-08 16:21:05 +0530

    update✏️

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { createReview, getReviewsByProduct, deleteReview, getAllReviews } from "../controllers/reviewController.js";
-import { requireAdmin, requireAuth } from "../controllers/middlewares/Authmiddleware.js";
+import { requireAuth } from "../controllers/middlewares/Authmiddleware.js";
 
 const Reviewrouter = express.Router();
 
 Reviewrouter.post("/", requireAuth, createReview);
 Reviewrouter.get('/',requireAuth,getAllReviews);
 Reviewrouter.get("/:productId", getReviewsByProduct);
-Reviewrouter.delete("/:reviewId",requireAuth, requireAdmin, deleteReview);
+Reviewrouter.delete("/:reviewId",requireAuth, deleteReview);
 
 export default Reviewrouter;
